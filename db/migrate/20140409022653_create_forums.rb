190c2cf1ca5c428c4d4988f60eee357566daf2d3
Mike Lat <mikelat@gmail.com>
2014-04-08 21:17:20 -0700

    making some controllers

---
+++ db/migrate/20140409022653_create_forums.rb
@@ -0,0 +1,12 @@
+class CreateForums < ActiveRecord::Migration
+  def change
+    create_table :forums do |t|
+      t.string :title
+      t.string :slug
+      t.text :description
+      t.integer :parent
+
+      t.timestamps
+    end
+  end
+end
