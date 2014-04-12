ed319ee95ebf43b3d1aed1307cdd7e18820133bf
Mike Lat <mikelat@gmail.com>
2014-04-12 15:32:02 -0700

    working on forum index

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
-gem 'bootstrap-sass'
+gem 'bootstrap-sass', '3.0.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/migrate/20140409022653_create_forums.rb
+++ db/migrate/20140409022653_create_forums.rb
@@ -5,8 +5,34 @@ class CreateForums < ActiveRecord::Migration
       t.string :slug
       t.text :description
       t.integer :parent
+      t.integer :order
 
       t.timestamps
+    end
+
+    add_index :forums, :slug, unique: true
+
+    change_column :forums, :order, :integer, default: 0
+    change_column :forums, :parent, :integer, default: 0
+
+    reversible do |dir|
+      # These default forums/parents for reset
+      dir.up do
+        category = Forum.new(:title => "Forum Category", :slug => 'category',
+                  :description => "A default category")
+
+        Forum.new(:title => "Test Forum", :slug => 'test',
+                  :description => "Look at me!", :parent => category.id,
+                  :order => 1)
+
+        Forum.new(:title => "Another Test Forum", :slug => 'another',
+                  :description => "Hurray :D", :parent => category.id,
+                  :order => 0)
+      end
+      # It's the first script, remove everything
+      dir.down do
+        Forum.delete_all()
+      end
     end
   end
 end
